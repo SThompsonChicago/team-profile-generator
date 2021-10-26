87eac1ec28ac9acb039934661cf33af959982cf1
Stephen Thompson <s31@umbc.edu>
2021-10-26 00:23:46 -0500

    Retrieve manager data from user

---
+++ index.js
@@ -0,0 +1,62 @@
+//Include packages
+const fs = require('fs');
+const inquirer = require('inquirer');
+//const Employee = require('employee');
+//const Manager = require('manager');
+
+const questions = [
+    {
+        message: "What is the team manager's name?",
+        name: "name",
+    },
+    {
+        message: "What is the team manager's ID?",
+        name: "ID",
+    },
+    {
+        message: "What is the team manager's email?",
+        name: "email",
+    },
+    {
+        message: "What is the team manager's office number?",
+        name: "officeNumber",
+    },
+];
+
+function generateHTML(data) {
+    return `<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UFT-8">
+        <title>My Team</title>
+    </head>
+    <body>
+        <p>${data.name}</p>
+        <p>Manager</p>
+        <p>ID: ${data.ID}</p>
+        <p>email: ${data.email}</p>
+        <p>Office number: ${data.officeNumber}<p>
+    </body>
+    </html>`;
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateHTML(data), (err) => {
+        err ? console.error(err) : console.log("Generating Team");
+    });
+}
+
+function init () {
+    inquirer.prompt(questions) 
+    .then((answers) => {
+        writeToFile('./index.html');
+    });
+}
+
+
+init();
+
+
+
+
+
